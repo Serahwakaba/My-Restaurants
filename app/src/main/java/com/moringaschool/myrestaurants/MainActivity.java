37476d740188a3bafc70dccc08a33528a2aa0c36
Serahwakaba <serahwakaba9@gmail.com>
2021-06-29 11:54:30 +0300

    binding with butterknife commit

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -4,29 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+    //using findviewbyid
+//    public static final String TAG = MainActivity.class.getSimpleName();
+//    private Button mFindRestaurantsButton;
+//    private EditText mLocationEditText;
+    //using butterknife
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+//    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+//        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+//              Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+//                Intent intent = getIntent();
+//                String location = intent.getStringExtra("location");
+                String location = mLocationEditText.getText().toString();
+//                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
             }
         });
     }
